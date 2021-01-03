dae62d779b36c4677f4c1b668355462b47f07067
kimberly <apps.rbi.nd@gmail.com>
2021-01-03 12:27:09 -0500

    first commit

---
+++ www/js/IntlTableSort-master/README.md
@@ -0,0 +1,23 @@
+# IntlTableSort
+
+HTML 5.3 International Table Sorter
+
+HTML 5.3: https://www.w3.org/TR/html53/
+
+Compatible list:
+
+Chrome 41+
+
+Edge 12+
+
+Firefox 44+
+
+Internet Explorer 11+
+
+Opera 25+
+
+Safari 10+
+
+Compatibility unknown:
+
+DOM 4.1: https://www.w3.org/TR/dom41/
---
+++ www/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_MaDhrsLhd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '52vcfjp7jma2t8mdglv0v8uruk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://km1nou9hod.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
